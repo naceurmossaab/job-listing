dca98de819ba106625e0461fd53f52dd8c65377b
naceurmossaab <naceurmossaab@gmail.com>
2025-06-11 03:44:01 +0100

    Implement navbar component with user authentication and navigation links

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { NgIf } from '@angular/common';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { RouterLink } from '@angular/router';
+import { AuthUser } from '../../models/auth';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgIf,
+    NzPageHeaderModule,
+    NzButtonModule,
+    NzMenuModule
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isAdmin: Boolean = false;
+  authUser?: AuthUser;
 
+  constructor(public authService: AuthService) {
+    if (!this.authUser)
+      this.authService.authUser$.subscribe(user => {
+        this.authUser = user
+        this.isAdmin = user?.role === 'admin'
+      });
+  }
+
+  ngOnInit(): void {
+    this.authService.isUserLoggedIn();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
